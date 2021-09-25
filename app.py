f8af9c4bac901354b6be1024868a620ed824c1e4
theBeginner86 <pranavsingh02@hotmail.com>
2021-09-25 18:03:50 +0530

    server added

---
+++ app.py
@@ -0,0 +1,34 @@
+import item_pb2
+import item_pb2_grpc
+import grpc
+
+import time
+from concurrent import futures
+
+
+class ItemService(item_pb2_grpc.ItemServiceServicer):
+    def Create(self, request, context):
+
+        request_value = {
+            'name': request.name,
+            'brand_name': request.brand_name,
+            'id': int(request.id),
+            'weight': request.weight
+        }
+        print(request_value)
+
+        return item_pb2.ItemMessage(**request_value)
+
+
+server = grpc.server(futures.ThreadPoolExecutor(max_workers=3))
+item_pb2_grpc.add_ItemServiceServicer_to_server(ItemService(), server)
+
+
+print('Server starting on port 5001')
+server.add_insecure_port('[::]:5001')
+server.start()
+try:
+    while True:
+        time.sleep(86400)
+except KeyboardInterrupt:
+    server.stop(0)
